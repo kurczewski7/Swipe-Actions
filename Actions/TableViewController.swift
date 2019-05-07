b0c12731c40033a9d29a50d1e5956f1e87bff799
Kurczewski Slawomir <kurczewski7@gmail.com>
2019-05-07 22:51:00 +0200

    Final commit

--- Actions/TableViewController.swift
+++ Actions/TableViewController.swift
@@ -23,28 +23,18 @@ final class Todo {
 }
 
 class TableViewController: UITableViewController {
-
     var todos = [Todo]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         for i in 1...10 {
             todos.append(Todo(title: "Todo #\(i)"))
         }
     }
-    
-    // Mark: - Table view delegate
-    
-    
-
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return todos.count
     }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
 
@@ -55,4 +45,50 @@ class TableViewController: UITableViewController {
 
         return cell
     }
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let important = importantAction(at: indexPath)
+        let delete = deleteAction(at: indexPath)
+        let swipeAction = UISwipeActionsConfiguration(actions: [important, delete])
+        return swipeAction
+    }
+    // MARK: - Swipe left and righi methods
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let checkAction = comleteAction(at: indexPath)
+        let swipeAction = UISwipeActionsConfiguration(actions: [checkAction])
+        return swipeAction
+    }
+    func importantAction(at indexPath: IndexPath) -> UIContextualAction {
+        let todo=todos[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "important") { (action, view, completion) in
+            print("importantAction")
+            todo.isImportant = !todo.isImportant
+            completion(true)
+        }
+        action.image = UIImage(named: "Alarm")
+        action.backgroundColor = (todo.isImportant ?  .purple : .gray)
+        return action
+    }
+    func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "delete") { (action, view, completion) in
+            print("deleteAction")
+            self.todos.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.image = UIImage(named: "Trash")
+        action.backgroundColor = .red
+        return action
+    }
+    func comleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "Complet") { (action, view, completion) in
+            print("comleteAction")
+            self.todos.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.image=UIImage(named: "Check")
+        action.backgroundColor = .green
+        return action
+    }
+
 }
